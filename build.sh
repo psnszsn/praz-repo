74709bb3628e3962f56f85639a4b76033bc9d3da
Vlad Pănăzan <vlad@panazan.ro>
2021-02-05 13:06:45 +0200

    move tlstunnel to aur.txt

--- build.sh
+++ build.sh
@@ -1,23 +1,42 @@
 #!/bin/sh
 set -ue
 
+export SRCDEST=/tmp/srcdest
+export BUILDDIR=/tmp/builddir
+
+REPODIR=$(pwd)
 AURPKGS=$(cat aur.txt)
 mkdir -p /tmp/aur
 mkdir -p /tmp/crepo
 
 cp /var/lib/pacman/sync/crepo.db /tmp/crepo
 ln -sf crepo.db /tmp/crepo/crepo.db.tar.gz
+
 cd /tmp/aur
-
 for pkg in ${AURPKGS} ; do
-
-	# $(echo $d | grep _) && continue;
 	echo _____
 	echo "$pkg"
 	paru -G "$pkg" --aur --nopgpfetch
 	cd "$pkg"
-	# ls -al
 
+	OLD_VERSION="$(pacman -Sl crepo | grep "$pkg" | awk '{print $3}' )"
+	NEW_VERSION="$(. ./PKGBUILD; echo ${pkgver}-${pkgrel})"
+
+	echo n: $NEW_VERSION o: ${OLD_VERSION:-0}
+	if test $(vercmp $NEW_VERSION ${OLD_VERSION:-r0}) -gt 0; then
+		echo Updating $pkg...
+		PKGDEST=/tmp/crepo makepkg -s --noconfirm --skippgpcheck
+	fi
+
+	cd ..
+done
+
+
+cd $REPODIR/pkgs
+for pkg in $(ls $REPODIR/pkgs) ; do
+	echo ____
+	echo "$pkg"
+	cd "$pkg"
 	OLD_VERSION="$(pacman -Sl crepo | grep "$pkg" | awk '{print $3}' )"
 	NEW_VERSION="$(. ./PKGBUILD; echo ${pkgver}-${pkgrel})"
 
@@ -32,5 +51,6 @@ done
 
 cd /tmp/crepo
 repo-add crepo.db.tar.gz *.pkg.tar.zst
+rm crepo.files*
 
 
