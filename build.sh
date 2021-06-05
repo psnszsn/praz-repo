c07389898ff4ccec9c69938a050214997d9c4673
Vlad Panazan <vlad@panazan.ro>
2021-06-05 17:32:53 +0000

    move to praz

--- build.sh
+++ build.sh
@@ -1,56 +1,46 @@
 #!/bin/sh
 set -ue
 
-export SRCDEST=/tmp/srcdest
-export BUILDDIR=/tmp/builddir
-
 REPODIR=$(pwd)
-AURPKGS=$(cat aur.txt)
-mkdir -p /tmp/aur
-mkdir -p /tmp/crepo
+export SRCDEST=$REPODIR/tmp/srcdest
+export BUILDDIR=$REPODIR/tmp/builddir
+export PKGDEST=/srv/http/repo.praz.xyz/arch
 
-cp /var/lib/pacman/sync/crepo.db /tmp/crepo
-ln -sf crepo.db /tmp/crepo/crepo.db.tar.gz
 
-cd /tmp/aur
-for pkg in ${AURPKGS} ; do
+build_dir() {
+	oldpath=$(pwd)
 	echo _____
-	echo "$pkg"
-	paru -G "$pkg" --aur --nopgpfetch
-	cd "$pkg"
-
-	OLD_VERSION="$(pacman -Sl crepo | grep "$pkg" | awk '{print $3}' )"
-	NEW_VERSION="$(. ./PKGBUILD; echo ${epoch:-0}:${pkgver}-${pkgrel})"
-
-	echo n: $NEW_VERSION o: ${OLD_VERSION:-0}
-	if test $(vercmp $NEW_VERSION ${OLD_VERSION:-r0}) -gt 0; then
-		echo Updating $pkg...
-		PKGDEST=/tmp/crepo makepkg -s --noconfirm --skippgpcheck
+	if [ ! -d "$1" ]; then
+		echo "DIR $1 does not exist"
+		exit 1
 	fi
+	cd "$1"
 
-	cd ..
-done
+    	echo Building $1...
+	makepkg -s --noconfirm --skippgpcheck || echo "???"
+	cd $oldpath
 
+}
 
-cd $REPODIR/pkgs
-for pkg in $(ls $REPODIR/pkgs) ; do
-	echo ____
-	echo "$pkg"
-	cd "$pkg"
-	OLD_VERSION="$(pacman -Sl crepo | grep "$pkg" | awk '{print $3}' )"
-	NEW_VERSION="$(. ./PKGBUILD; echo ${epoch:-0}:${pkgver}-${pkgrel})"
+build_aur() {
+	AURPKGS=$(cat aur.txt)
+	mkdir -p /tmp/aur
+	cd /tmp/aur
+	for pkg in ${AURPKGS} ; do
+		echo "Downloading $pkg"
+		paru -G "$pkg" --aur --nopgpfetch
+		build_dir $pkg
+	done
+	cd $REPODIR
+}
 
-	echo n: $NEW_VERSION o: ${OLD_VERSION:-a}
-	if test $(vercmp $NEW_VERSION ${OLD_VERSION:-a}) -gt 0; then
-		echo Updating $pkg...
-		PKGDEST=/tmp/crepo makepkg -s --noconfirm --skippgpcheck
-	fi
+build_local() {
+	for pkg in $(ls $REPODIR/pkgs) ; do
+		build_dir pkgs/$pkg
+	done
 
-	cd ..
-done
+}
 
-cd /tmp/crepo
-repo-add crepo.db.tar.gz *.pkg.tar.zst
-rm crepo.files*
-
+build_local
+build_aur
 
